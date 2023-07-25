d3526af45f3b78725ff27c4a258b943de479c0ce
Marroks09 <140541037+Marroks09@users.noreply.github.com>
2023-07-25 11:17:02 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,35 @@
+function msj_salir(){
+     //algoritmo de la función
+     alert("Eres un crackkkkkkkkkkkkkkk")
+}
+
+function invitar(){
+     var nombre;
+     var deseo;
+
+     nombre=prompt("Escribe tu nombre: ")
+     deseo=prompt("¿Que te gusta comer?")
+
+     alert("¡Hola! "+nombre+"te invito a comer "+deseo)
+}
+
+function invitar(){
+     //algoritmo de la función
+     alert("Eres un CRACKK")
+    }
+//Si la respuesta es correcta
+function adivinar(){
+     var respuesta;
+     respuesta=prompt("¿Que tiene el rey en la panza?")
+
+     if(respuesta=="ombligo"){ 
+          alert("¡CORRECTO!")
+ }
+ //Si es falso
+ else{
+     alert("¡ERROR!")
+ }
+     }
+function llevar(){
+        window.open("https://www.youtube.com/watch?v=09m0B8RRiEE", "Video1", "width=500px height=500px")
+}
